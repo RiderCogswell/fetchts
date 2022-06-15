fcc057659c46c2082e263064c93958f5c75e26d9
Ridercogswell <ridercogswell@gmail.com>
2022-06-14 22:51:37 -0700

    interface

--- index.ts
+++ index.ts
@@ -3,15 +3,24 @@ import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-  const todo = response.data;
+  // as is linking the interface to the response data
+  const todo = response.data as Todo;
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  // this is us telling typescript what were working with
+  interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+
+  // must be direct and exactly as data is stated.
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     Has a title of: ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `);
 });
